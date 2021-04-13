03c62cddb93060ca60542240a9fd99623284a7bb
Vladimir Volek <vladimir.volek@satoshilabs.com>
2021-04-13 20:04:36 +0200

    fix: leftovers

--- example/index.ts
+++ example/index.ts
@@ -1,14 +1,11 @@
 import { BlockFrostAPI } from '../src/index';
-
 async function run() {
   const API = new BlockFrostAPI({
-    projectId: 'v4yhZTgZRjJJbLAPMOmMGtZFg3MGIWiT', // see: https://blockfrost.io
+    projectId: 'YOUR API KEY HERE', // see: https://blockfrost.io
   });
 
   try {
-    const response = await API.txs(
-      '28172ea876c3d1e691284e5179fae2feb3e69d7d41e43f8023dc380115741026',
-    );
+    const response = await API.blocksLatest();
     console.log(response);
   } catch (err) {
     console.log('error', err.data);
