e31897577a5184226b19b34fed2b1078d65a77b0
Vladimir Volek <vladimir.volek@satoshilabs.com>
2021-04-27 10:23:46 +0200

    fix: example

--- example/index.ts
+++ example/index.ts
@@ -1,13 +1,12 @@
 import { BlockFrostAPI } from '../src/index';
+
 async function run() {
   const API = new BlockFrostAPI({
-    projectId: 'v4yhZTgZRjJJbLAPMOmMGtZFg3MGIWiT', // see: https://blockfrost.io
+    projectId: 'YOUR API KEY HERE', // see: https://blockfrost.io
   });
 
   try {
-    const response = await API.addressesUtxosAll(
-      'addr1qxszx202k6d9hxdfgufta6x6grr9czk4m8kvmgdrg0arut8e32c7akdgzud56kzamm99gtfpq2y3qyfzjj9hrwr0ka4q04xdyq',
-    );
+    const response = await API.blocksLatest();
     console.log(response);
   } catch (err) {
     console.log('error', err.data);
