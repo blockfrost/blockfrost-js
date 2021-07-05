227d5e98155771ec437d24023132d51c5cea4dd7
Vladimir Volek <volek.vladimir@gmail.com>
2021-07-05 16:53:49 +0200

    tests: add more tests

---
+++ test/fixtures/assets.ts
@@ -0,0 +1,507 @@
+import { SDK } from '../utils';
+
+export default [
+  {
+    command: () => SDK.assets(),
+    response: [
+      {
+        asset:
+          '00000002df633853f6a47465c9496721d2d5b1291b8398016c0e87ae6e7574636f696e',
+        quantity: '1',
+      },
+      {
+        asset:
+          '3a9241cd79895e3a8d65261b40077d4437ce71e9d7c8c6c00e3f658e4669727374636f696e',
+        quantity: '1',
+      },
+      {
+        asset:
+          '02f68378e37af4545d027d0a9fa5581ac682897a3fc1f6d8f936ed2b4154414441',
+        quantity: '1000000',
+      },
+      {
+        asset:
+          'e8e62d329e73190190c3e323fb5c9fb98ee55f0676332ba949f29d724649525354',
+        quantity: '1',
+      },
+      {
+        asset:
+          'ac3f4224723e2ed9d166478662f6e48bae9ddf0fc5ee58f54f6c322943454e54',
+        quantity: '10000000',
+      },
+      {
+        asset:
+          '12e65fa3585d80cba39dcf4f59363bb68b77f9d3c0784734427b151754534c41',
+        quantity: '425839369',
+      },
+      {
+        asset:
+          'e12ab5cf12f95cd57b739282d06af9dd61e1b1dde1e06f0c31f0251167696d62616c',
+        quantity: '42',
+      },
+      {
+        asset: 'da8c30857834c6ae7203935b89278c532b3995245295456f993e1d244c51',
+        quantity: '21000000000000',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279416c6261',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279416d657468797374',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279417175616d6172696e65',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279417368',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727941756275726e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279417572656c6961',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279417572656f6c696e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279417765736f6d65',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279417a756c',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794265696765',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279426572796c',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279426c61636b',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279426c7565',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279426f6e65',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279427269636b',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727942726f776e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727942797a616e74696e65',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794361646574',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727943616d656c',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794361707269',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727943617264696e616c',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279436572756c65616e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727943686172747265757365',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279436865727279',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727943696e6e616d6f6e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279436c656d656e74696e65',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279436f616c',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279436f62616c74',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279436f66666565',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279436f72616c',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794379616e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727945626f6e79',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279456d6572616c64',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727946756368736961',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279476f6c64',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727947726179',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279477265656e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727948617a656c',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727948756d62726f6c',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279496e6469676f',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727949726973',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794a616465',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794a657474',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794b656c6c79',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794b68616b69',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794c617661',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794c6176656e646572',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794c656d6f6e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794c696d65',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794c757374',
+        quantity: '1',
+      },
+      {
+        asset: 'ca079f39e33ca1b6ec7b4f4eedffa4f40d7d111f96bde855ba224dc2444558',
+        quantity: '10000000',
+      },
+      {
+        asset:
+          '12e65fa3585d80cba39dcf4f59363bb68b77f9d3c0784734427b15174141504c',
+        quantity: '9648589196',
+      },
+      {
+        asset:
+          'b93822810dbd56a5b5b815b1946da690312913da74c94e019e028af97065646572',
+        quantity: '5',
+      },
+      {
+        asset:
+          '12e65fa3585d80cba39dcf4f59363bb68b77f9d3c0784734427b15174150504c45',
+        quantity: '9648589196',
+      },
+      {
+        asset:
+          'd63b50fe629b69f1da4897aee9f381fe197c30c06bf35be4355360bd6d617279',
+        quantity: '1',
+      },
+      {
+        asset:
+          '12e65fa3585d80cba39dcf4f59363bb68b77f9d3c0784734427b15175445534c41',
+        quantity: '425839369',
+      },
+      {
+        asset:
+          'd3501d9531fcc25e3ca4b6429318c2cc374dbdbcf5e99c1c1e5da1ff444f4e545350414d',
+        quantity: '10000000',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d61726f6f6e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d61757665',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d656c616e6965',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d656c6f6e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d696e74',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d6f7261646f',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d6f7373',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d757374617264',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794d7972746c65',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794e617679',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794f6c697665',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794f6e7978',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794f72616e6765',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272794f7263686964',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272795065616368',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727950656172',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727950696e6b',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727950697374616368696f',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279506c6174696e756d',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279507572706c65',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272795261636b6c6579',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279526173706265727279',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279526176656e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279526564',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279526f7365',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279526f7578',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727952756279',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279527573736574',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272795275737479',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727953616666726f6e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727953616765',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727953616c6d6f6e',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f426572727953616e64',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f42657272795361707068697265',
+        quantity: '1',
+      },
+      {
+        asset:
+          'b863bc7369f46136ac1048adb2fa7dae3af944c3bbb2be2f216a8d4f4265727279536361726c657474',
+        quantity: '1',
+      },
+    ],
+  },
+] as const;
