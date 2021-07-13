b8b1fc7392715149e8088d37c4852561c12ac4dc
Vladimir Volek <volek.vladimir@gmail.com>
2021-07-13 16:37:34 +0200

    ipfs: add more tests

--- test/tests/ipfs/index.ts
+++ test/tests/ipfs/index.ts
@@ -1,7 +1,7 @@
 import { IPFS } from '../../utils';
 
 describe('IPFS', () => {
-  test('add + pin + list + remove', async () => {
+  test('flow', async () => {
     const addedObject = await IPFS.add({
       path: 'https://i.imgur.com/tEaBDoq.jpeg',
       sourceType: 'url',
@@ -12,7 +12,22 @@ describe('IPFS', () => {
     const pinnedObject = await IPFS.pin(addedObject.cid);
 
     expect(pinnedObject).toMatchObject({
-      ipfs_hash: expect.any(String), state: expect.any(String)
+      ipfs_hash: expect.any(String),
+      state: expect.any(String),
     });
+
+    const list = await IPFS.list(addedObject.cid);
+
+    expect(list).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          time_created: expect.any(Number),
+          time_pinned: expect.any(Number),
+          ipfs_hash: expect.any(String),
+          size: expect.any(String),
+          state: expect.any(String),
+        }),
+      ]),
+    );
   });
 });
