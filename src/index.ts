eddc5f1eee4f8c830cd7e4003967d0b69f40e5eb
Vladimir Volek <volek.vladimir@gmail.com>
2021-08-24 14:39:30 +0200

    feat: asset utils

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { components } from './types/OpenApi';
 import { ErrorType } from './types';
 import { BlockFrostAPI } from './BlockFrostAPI';
 import { BlockFrostIPFS } from './BlockFrostIPFS';
+import { parseAsset, getFingerprint } from './utils/index';
 
 type Responses = components['schemas'];
 type Error = ErrorType;
 
-export { BlockFrostAPI, BlockFrostIPFS };
+export { BlockFrostAPI, BlockFrostIPFS, parseAsset, getFingerprint };
 
 export type { Responses, Error };
