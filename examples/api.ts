b61c890f224db92b7470bdb544388e5424fe652e
Vladimir Volek <volek.vladimir@gmail.com>
2021-09-21 13:12:39 +0200

    chore: add example

--- examples/api.ts
+++ examples/api.ts
@@ -13,7 +13,9 @@ async function run() {
     const address = await API.addresses(
       'addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz',
     );
+    const pools = await API.pools({ page: 1, count: 10, order: 'asc' });
 
+    console.log('pools', pools);
     console.log('address', address);
     console.log('networkInfo', networkInfo);
     console.log('latestEpoch', latestEpoch);
