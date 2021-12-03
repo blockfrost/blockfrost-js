0be1a1ecf1a39c9cbd9e4246ea7a6e1ab2dec61d
Vladimir Volek <volek.vladimir@gmail.com>
2021-12-03 14:23:39 +0100

    fix: tests

--- test/fixtures/account/index.ts
+++ test/fixtures/account/index.ts
@@ -4,6 +4,7 @@ export const deriveAddressFixtures = [
       '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
     index: 0,
     type: 0,
+    isTestnet: false,
     response: {
       address:
         'addr1q8u5ktsj5zsmhvwv0ep9zuhfu39x3wyt9wxjnsn3cagsyy59ckxhkvuc5xj49rw6zrp443wlygmhv8gwcu38jk6ms6usrmcafl',
@@ -15,9 +16,34 @@ export const deriveAddressFixtures = [
       '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
     index: 1,
     type: 0,
+    isTestnet: false,
     response: {
       address:
         'addr1qxnthyxq8x9lv95h74k5av3sy3yzljr56ttxu4lggv8qstv9ckxhkvuc5xj49rw6zrp443wlygmhv8gwcu38jk6ms6us8mueja',
+      path: "m/1852'/1815'/0'/0/1",
+    },
+  },
+  {
+    publicKey:
+      '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
+    index: 0,
+    type: 0,
+    isTestnet: true,
+    response: {
+      address:
+        'addr_test1qru5ktsj5zsmhvwv0ep9zuhfu39x3wyt9wxjnsn3cagsyy59ckxhkvuc5xj49rw6zrp443wlygmhv8gwcu38jk6ms6usqd9a9q',
+      path: "m/1852'/1815'/0'/0/0",
+    },
+  },
+  {
+    publicKey:
+      '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
+    index: 1,
+    type: 0,
+    isTestnet: true,
+    response: {
+      address:
+        'addr_test1qznthyxq8x9lv95h74k5av3sy3yzljr56ttxu4lggv8qstv9ckxhkvuc5xj49rw6zrp443wlygmhv8gwcu38jk6ms6usydpe7z',
       path: "m/1852'/1815'/0'/0/1",
     },
   },
@@ -28,6 +54,7 @@ export const getAccountFixtures = [
     publicKey:
       '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
     type: 0,
+    isTestnet: false,
     response: [
       {
         address:
@@ -289,6 +316,7 @@ export const getAccountFixtures = [
     publicKey:
       '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
     type: 1,
+    isTestnet: false,
     response: [
       {
         address:
