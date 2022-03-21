06a1a0fc1f45f072584558a3c1a9606288836d1d
slowbackspace <slowbackspace@gmail.com>
2022-03-21 11:48:43 +0100

    chore: expose rate limiter from class

--- src/utils/got.ts
+++ src/utils/got.ts
@@ -1,25 +1,21 @@
 import got, { Got } from 'got';
 import Bottleneck from 'bottleneck';
 import { ValidatedOptions } from '../types';
-import { getLimiter } from './limiter';
 
 export const getInstance = (
   apiUrl: string,
   options: ValidatedOptions,
   userAgent: string | undefined,
+  rateLimiter: Bottleneck | undefined,
 ): Got => {
-  let limiterQueue: Bottleneck;
-  if (options.rateLimiter) {
-    limiterQueue = getLimiter(options.rateLimiter);
-  }
-
   return got.extend({
     hooks: {
       beforeRequest: [
         async hookOptions => {
-          if (options.rateLimiter) {
-            await limiterQueue.schedule(() => Promise.resolve(true));
+          if (rateLimiter) {
+            await rateLimiter.schedule(() => Promise.resolve(true));
           }
+
           if (options.debug) {
             console.log(`${hookOptions.method} ${hookOptions.url}`);
           }
