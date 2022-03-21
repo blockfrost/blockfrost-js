fa470acc7b5ccf0f09259b7905c64bb3634b7ca4
slowbackspace <slowbackspace@gmail.com>
2022-03-21 11:01:12 +0100

    chore: increase jest timeout for /assets  test

--- test/types.ts
+++ test/types.ts
@@ -3,6 +3,7 @@ import { BlockFrostAPI } from '../src';
 export interface TestFixture {
   name: string;
   command: (SDK: BlockFrostAPI) => any;
+  jestTimeout?: number;
   response: any;
   itemsCountMinimum?: number;
   itemsCount?: number;
