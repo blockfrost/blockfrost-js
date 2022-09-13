f2c00d33987de142471718a3e12abb4817404105
slowbackspace <slowbackspace@gmail.com>
2022-09-13 14:42:32 +0200

    chore: add integration test for tx submit error

--- packages/blockfrost-js/test/fixtures/endpoints/txs.ts
+++ packages/blockfrost-js/test/fixtures/endpoints/txs.ts
@@ -1,3 +1,4 @@
+import { expect } from '@jest/globals';
 import { BlockFrostAPI } from '../../../src';
 
 export default [
@@ -22,6 +23,32 @@ export default [
       stake_cert_count: 0,
       pool_update_count: 0,
       pool_retire_count: 0,
+    },
+  },
+  {
+    command: (SDK: BlockFrostAPI) => {
+      return new Promise(resolve => {
+        SDK.txSubmit(
+          '84a4008b82582092ebc1cecef689a0acc6a9b4a961340d90b157dc55b3aafe4b9d2bb9a4d35edd028258202e28b46060ec4d40c84eae389cdb9202347e3d524a20bf943079edd3b1a66cd002825820c718f0420681a1f4d5a3571cde31904e4e18387a9eeb9c3916a90449456f37e402825820b4c666e611e446712f7dd68450510f95ced76d343e3dc331f2f6def0340ba3ff028258205f8e735ea516c0a217736ea204602d8ff01e21e5d137b464025616fc0fcddeaa02825820b4a4c1bde03845ede909aa1e22f375349f6e812cbf9f8bf1e86e2f35864cba7f02825820c22cdbd695ca30e3842c23368d6e32768f53cbe1fc8642348d2eb2ec6448a09e0282582004bbfaf0036f4e14c7b6cc78bf4e26980b9e058ef37160479f9ee4ed2d4c22b6028258204f06620e516aba68fa9ea1f6f1b6b87cd7d5a83c4d3055eb5e96519b137e37a20282582010fecd3c8f6297ee20ef55fc46a1c76001e370c1b66210efaaa5c2d3f17e0cd902825820a8df2ed8de772fdb7ec77f00bc73e1e2fdf913b64202ee4e950c6757d404347f01018282584c82d818584283581c15cf80b94b8f984636a23214bcdc589697561bd040ad592f9db9b53ea101581e581c2b0b011ba3683d6742f2fa2a7725ac578b5f593b7d287c47526bf9f1001a8c1cf2611a39c1718582583901abcfe464de63170a4bb97acde2f29964f1de91d90cdd0bbdc80b61ff138b7cdb01acc61a6cbdbd1f7fcac84980ec1f3e8d709fa8d09f462f821a00161934a1581cf0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9aa24779616d666f726501497a617274736e61726601021a0002ed19031a04394beda100828258201fc31428a0c21d4f0428c289382703de0f0bec374a70b7566154e01379def152584095f950f5b551e96b68695f4791f4f831a986ea30fc7eb2cc644077e6c965c9ebcc75c9e5fd8ee9a8e0c2cf077e6622ea2921e56f68b49640535849f03cf7e507825820c063ca2d228605a9c45a94ed876c9f638a9e362974a2edeaf882feb657e7a4835840b2292ae3c52bd659e393b8128a553b77fc26c1ff1ef6fe41c3b936b2696a4ec6c589e577ed337b489a25cbcfaea4489e07c91c3b650fa39dda9458390d736802f5f6',
+        )
+          .then(data => {
+            resolve(data);
+          })
+          .catch(err => {
+            resolve({
+              error: err.error,
+              status_code: err.status_code,
+              message: err.message,
+            });
+          });
+      });
+    },
+    response: {
+      error: 'Bad Request',
+      message: expect.stringMatching(
+        new RegExp(/.*transaction submit error.*/),
+      ),
+      status_code: 400,
     },
   },
   {
